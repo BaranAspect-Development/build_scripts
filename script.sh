dba708509d32c7ecdefed8ef01632658fdea6625
Muhammad Masood <m.masood7679@gmail.com>
2025-03-16 22:46:16 +0500

    topaz: lineageOs script

--- script.sh
+++ script.sh
@@ -3,13 +3,13 @@
 rm -rf .repo/local_manifests/
 
 # repo init rom
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 15 --git-lfs
+repo init -u https://github.com/Masood-J/android_lineage.git -b lineage-22.2 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Local manifests
-git clone https://github.com/BaranAspect-Development/rom-build.git -b derp-15 .repo/local_manifests
+git clone https://github.com/BaranAspect-Development/rom-build.git -b evo-x .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -37,8 +37,12 @@ echo "==========================="
 export BUILD_USERNAME=Masood
 export BUILD_HOSTNAME=crave
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+
 # initiate build setup
 . build/envsetup.sh
 
+export WITH_GMS := false
+
+
 echo "======= Export Done ======"
-lunch derp_topaz-user && mka derp
+lunch lineage_topaz-bp1a-userdebug && mka bacon || brunch a10 userdebug
